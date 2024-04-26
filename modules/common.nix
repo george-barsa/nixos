cd6c63f0de0bd91a82d47a0f7bca937f1592ca34
george-barsa <117371911+george-barsa@users.noreply.github.com>
2024-04-26 16:52:56 -0400

    updated syncthing

--- modules/common.nix
+++ modules/common.nix
@@ -100,6 +100,33 @@
   #  wget
   # ];
 
+  services = {
+    syncthing = {
+      enable = true;
+      user = "${vars.user}";
+      dataDir = "/home/${vars.user}/Documents";
+      configDir = "/home/${vars.user}/Documents/.config/syncthing";
+      overrideDevices = true;
+      overrideFolders = true;
+      settings = {
+        devices = {
+          "nixos-worktop" = { id = "F2WEJDI-6ZFMTUK-FN2AGHL-J3CJZQL-YXSRYBH-GRKFS3N-SBTX5QP-6CLFZAQ"; };
+          "george-pixel6a" = { id = "PJ7UURK-ZSDAEF6-BCIX5LG-CAHDGO4-2B7NXKU-FNEHIVB-VHSWAZ4-JDNVBQZ"; };
+        };
+        folders = {
+          "07tdg-wgfb5" = {
+            label = "Logseq";
+            path = "/home/${vars.user}/Documents/logseq";
+            devices = [ 
+              "nixos-worktop" 
+              "george-pixel6a" 
+            ];
+          };
+        };
+      };
+    };
+  };
+    
   environment.variables.EDITOR = "hx";
 
   programs.steam = {
