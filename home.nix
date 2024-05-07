45ef6e240cd69749176cfc377bf15e7ab6beb9eb
george-barsa <117371911+george-barsa@users.noreply.github.com>
2024-05-07 12:42:35 -0400

    theme

--- home.nix
+++ home.nix
@@ -25,7 +25,7 @@
 
   programs.kitty = {
     enable = true;
-    theme = "GitHub Dark";  
+    theme = "Gruvbox Dark";  
     settings.symbolMap = "U+23FB-U+23FE,U+2665,U+26A1,U+2B58,U+E000-U+E00A,U+E0A0-U+E0A3,U+E0B0-U+E0D4,U+E200-U+E2A9,U+E300-U+E3E3,U+E5FA-U+E6AA,U+E700-U+E7C5,U+EA60-U+EBEB,U+F000-U+F2E0,U+F300-U+F32F,U+F400-U+F4A9,U+F500-U+F8FF,U+F0001-U+F1AF0 Symbols Nerd Font Mono";
   };
   
@@ -46,7 +46,7 @@
           left = ["mode" "spinner" "version-control" "file-name" "read-only-indicator" "file-modification-indicator"];
         };
       };
-      theme = "github_dark";
+      theme = "gruvbox";
     };
   };
 
