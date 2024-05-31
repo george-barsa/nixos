5dd6ce5253562a9b0fee5b8539c63d3278f89baa
george-barsa <117371911+george-barsa@users.noreply.github.com>
2024-05-31 13:58:20 -0400

    upgrade from 24.11 to 24.05

--- home.nix
+++ home.nix
@@ -134,7 +134,7 @@
   # You can update home Manager without changing this value. See
   # the home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "23.11";
+  home.stateVersion = "24.05";
 
   programs.home-manager.enable = true;
 }
