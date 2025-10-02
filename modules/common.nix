33c1f56ce860961a5d71228a81cf73512f7f6325
george-barsa <117371911+george-barsa@users.noreply.github.com>
2025-10-02 14:47:06 -0400

    docker

--- modules/common.nix
+++ modules/common.nix
@@ -126,6 +126,8 @@
     dedicatedServer.openFirewall = true;
   };
 
+  services.docker.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
